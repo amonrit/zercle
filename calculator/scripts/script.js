59593c66cb9aa35415f7660a552bb76d9cbcd44c
amonrit <76424455+amonrit@users.noreply.github.com>
2022-05-11 20:48:56 +0700

    update

---
+++ calculator/scripts/script.js
@@ -0,0 +1,53 @@
+function fun_answer() {
+  document.getElementById("fieldname").value = eval(
+    document.getElementById("fieldname").value
+  );
+}
+function fun0() {
+  document.getElementById("fieldname").value += "0";
+}
+function fun1() {
+  document.getElementById("fieldname").value += "1";
+}
+function fun2() {
+  document.getElementById("fieldname").value += "2";
+}
+function fun3() {
+  document.getElementById("fieldname").value += "3";
+}
+function fun4() {
+  document.getElementById("fieldname").value += "4";
+}
+function fun5() {
+  document.getElementById("fieldname").value += "5";
+}
+function fun6() {
+  document.getElementById("fieldname").value += "6";
+}
+function fun7() {
+  document.getElementById("fieldname").value += "7";
+}
+function fun8() {
+  document.getElementById("fieldname").value += "8";
+}
+function fun9() {
+  document.getElementById("fieldname").value += "9";
+}
+function fun_c() {
+  document.getElementById("fieldname").value = "";
+}
+function fun_dot() {
+  document.getElementById("fieldname").value += ".";
+}
+function fun_plus() {
+  document.getElementById("fieldname").value += "+";
+}
+function fun_minus() {
+    document.getElementById("fieldname").value += "-";
+  }
+function fun_multiply() {
+    document.getElementById("fieldname").value += "*";
+  }
+function fun_divide() {
+    document.getElementById("fieldname").value += "/";
+  }
